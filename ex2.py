ba0275f0dc168fd732d36e18e1dd0e5043570420
CeJinn <1318104160@qq.com>
2020-04-06 10:06:40 +0800

    practice 2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+datapre = pd.read_table("C:\\Users\\asus\\Desktop\\大三下\\媒体数据管理\\ColorHistogram.asc", header=None)
+# data = datapre.values.tolist()
+data = datapre.values
+print(data)
